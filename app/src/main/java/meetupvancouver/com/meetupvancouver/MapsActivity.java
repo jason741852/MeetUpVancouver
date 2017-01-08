1e82e4dd6b7060ea8318284c5a869f919c6274f7
Jason Chan <jason741852@gmail.com>
2017-01-08 00:26:56 -0800

    added buttons to bring from map to host event details

--- app/src/main/java/meetupvancouver/com/meetupvancouver/MapsActivity.java
+++ app/src/main/java/meetupvancouver/com/meetupvancouver/MapsActivity.java
@@ -7,10 +7,17 @@ import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.util.Log;
+
+
 import android.content.Intent;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -23,6 +30,9 @@ public class MapsActivity extends AppCompatActivity
     private TextView mTapTextView;
     private TextView mCameraTextView;
     private GoogleMap mMap;
+    Marker marker;
+    Button hostButton;
+    LatLng hostPoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +41,22 @@ public class MapsActivity extends AppCompatActivity
 
         mTapTextView = (TextView) findViewById(R.id.tap_text);
         mCameraTextView = (TextView) findViewById(R.id.camera_text);
+        hostButton = (Button) findViewById(R.id.hostButton);
 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        hostButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d("hi","button clicked");
+                Intent i = new Intent(MapsActivity.this, EventDetails.class);
+                i.putExtra("point",hostPoint);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
@@ -47,13 +69,25 @@ public class MapsActivity extends AppCompatActivity
 
     @Override
     public void onMapClick(LatLng point) {
+
         mTapTextView.setText("tapped, point=" + point);
+        if (marker != null) {
+            marker.remove();
+        }
+        marker = mMap.addMarker(new MarkerOptions().position(point));
+        hostPoint = point;
     }
 
     @Override
     public void onMapLongClick(LatLng point) {
 
         mTapTextView.setText("long pressed, point=" + point);
+        if (marker != null) {
+            marker.remove();
+        }
+        marker = mMap.addMarker(new MarkerOptions().position(point));
+        hostPoint = point;
+
     }
 
     @Override
