b755a07c200bffc5d45c6728737184355afc33ca
Jason Chan <jason741852@gmail.com>
2017-01-08 04:53:30 -0800

    jason created initial event display page

--- app/src/main/java/meetupvancouver/com/meetupvancouver/ShowEvent.java
+++ app/src/main/java/meetupvancouver/com/meetupvancouver/ShowEvent.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class ShowEvent extends AppCompatActivity {
+
+    TextView eventView, dateView, descView, hostView, timeView;
 
 
     @Override
@@ -17,11 +20,23 @@ public class ShowEvent extends AppCompatActivity {
         Log.d("FULL string",info);
         String[] infoTokens;
 
-//        infoTokens = info.split("\\$");
+        infoTokens = info.split("\\$");
 //        for(Integer i=0; i<infoTokens.length;i++){
 //            Log.d("tokens",infoTokens[i]);
 //        }
 //
+        eventView = (TextView) findViewById(R.id.eventText);
+        dateView = (TextView) findViewById(R.id.dateText);
+        descView = (TextView) findViewById(R.id.descText);
+        hostView = (TextView) findViewById(R.id.hostText);
+        timeView = (TextView) findViewById(R.id.timeText);
+
+
+        eventView.setText(infoTokens[0]);
+        dateView.setText(infoTokens[1]);
+        descView.setText(infoTokens[2]);
+        hostView.setText(infoTokens[3]);
+        timeView.setText(infoTokens[4]);
 
 
 
